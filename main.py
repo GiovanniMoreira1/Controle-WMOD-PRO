b8fd6022e8eec829dad4d6e532f4e537751b16fc
Giovanni <giovannimoreira131@gmail.com>
2025-11-26 11:34:14 -0300

    criação do script sql para o db, conexão com o banco de dados ok

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from credentials import DB_PASSWORD, DB_USER
 import tkinter as tk
 from tkinter import ttk
+import psycopg
+
+
+with psycopg.connect(f"dbname=postgres user={DB_USER} password={DB_PASSWORD}") as conn:
+    with conn.cursor() as cur:
+
+        cur.execute(
+            "INSERT INTO funcionarios (nome, email) VALUES (%s, %s)", ("teste", "teste@durr.com.br")
+        )
+
+        cur.execute("SELECT * FROM funcionarios")
+        print(cur.fetchone())
+
+        conn.commit()
+
 
 root = tk.Tk()
 ttk.Button(root, text="hello world").grid()
